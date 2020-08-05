2f1c83cbff9a4e6069264f91c2669848b632400c
Blue Abad <bibo1011@gmail.com>
2020-08-04 20:28:22 -0700

    add round counter in the battle

--- assets/js/game.js
+++ assets/js/game.js
@@ -56,10 +56,25 @@ var fight = function(enemyName) {
 };
 
   for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      //let user know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+      window.alert("Welcome to Robot Gladiator! Round" + ( i + 1 ));
+    } else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
+    // pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyName = enemyNames[i];
+    //reset enemyHealth before starting a new fight
     enemyHealth = 50;
-    // call fight function with enemy robot
+
+    //use debugger to pause script from running and check what's going on at that moment in the code
+    // debugger;
+
+    // pass the pickedEnemyName variable's value into the fight function where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
+
+    
   }
 //fight();
 
